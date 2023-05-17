b406b9d6cff127e0992d0592d0f19fa59a0db031
Samuel Rae <samuelrae@Samuels-Air.fritz.box>
2023-05-17 08:41:15 +0100

    test: add jest

--- script.ts
+++ script.ts
@@ -1,1 +1,6 @@
-process.stdout.write("hi\n");
+export function sum(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+const result = sum(2, 2);
+process.stdout.write(`The sum of 2 and 2 is ${result}\n`);
